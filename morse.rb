1088dc79f82171388d2613f7d06fe0a098919a2e
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-23 10:37:30 +0100

    complete morse decode message

---
+++ morse.rb
@@ -0,0 +1,25 @@
+def decode_char(string)
+    morse_code = {
+      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+      '--..' => 'Z'
+    }
+    return morse_code[string]
+  end
+
+  def decode_word(word)
+    result = "";
+    word.split.each { |n| result += decode_char(n)}
+    return result
+  end
+
+  def decode(content)
+    result = " "
+    content.split("   ").each { |n| result += "#{decode_word(n)} " }
+    return result
+  end
+
+
