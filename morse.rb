7048c7522631a1d1e951793190a7fb3915241fb7
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-23 10:53:01 +0100

    fix rubocop errors

--- morse.rb
+++ morse.rb
@@ -1,25 +1,25 @@
+# frozen_string_literal: true
+
 def decode_char(string)
-    morse_code = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-      '--..' => 'Z'
-    }
-    return morse_code[string]
-  end
+  morse_code = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_code[string]
+end
 
-  def decode_word(word)
-    result = "";
-    word.split.each { |n| result += decode_char(n)}
-    return result
-  end
+def decode_word(word)
+  result = ''
+  word.split.each { |n| result += decode_char(n) }
+  result
+end
 
-  def decode(content)
-    result = " "
-    content.split("   ").each { |n| result += "#{decode_word(n)} " }
-    return result
-  end
-
-
+def decode(content)
+  result = ' '
+  content.split('   ').each { |n| result += "#{decode_word(n)} " }
+  result
+end
